1bffbc53e8e3e214a1867fd4ff1e0007b9246754
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-12 18:12:56 -0600

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -13,30 +13,30 @@
 
 // EXCITED KITTEN
 
-catSpeak = "Love me, pet me! HSSSSSS!";
-catSpeak1 = "...human...why you taking pictures of me?...";
-catSpeak2 = "...the catnip made me do it..."
-catSpeak3 = "...why does the red dot always get away...";
+// catSpeak = "Love me, pet me! HSSSSSS!";
+// catSpeak1 = "...human...why you taking pictures of me?...";
+// catSpeak2 = "...the catnip made me do it..."
+// catSpeak3 = "...why does the red dot always get away...";
 
-for(let i = 1; i < 20; i++){
-    if(i % 2 != 0){
-        console.log(catSpeak);
-    }
-    Math.random();
-    randomMath = Math.random();
-    if(i % 2 === 0){
-        if(randomMath <= .33){
-            console.log(catSpeak1);
-        } else if(randomMath > .33 && randomMath <= .66){
-            console.log(catSpeak2)
-        } else if(randomMath >= .66){
-            console.log(catSpeak3);
-        }
-    }
+// for(let i = 1; i < 20; i++){
+//     if(i % 2 != 0){
+//         console.log(catSpeak);
+//     }
+//     Math.random();
+//     randomMath = Math.random();
+//     if(i % 2 === 0){
+//         if(randomMath <= .33){
+//             console.log(catSpeak1);
+//         } else if(randomMath > .33 && randomMath <= .66){
+//             console.log(catSpeak2)
+//         } else if(randomMath >= .66){
+//             console.log(catSpeak3);
+//         }
+//     }
 
-} 
+// } 
 
-// GETTING TO KNOW YOU
+// FIZZ BUZZ
 
 // for(i = 1; i <= 100; i++){
 //     if(i % 3 === 0){
@@ -50,6 +50,27 @@ for(let i = 1; i < 20; i++){
 //     }
         
 // }
+
+// GETTING TO KNOW YOU
+
+
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+shahzad.splice(0,1,'Gameboy');
+daniel[1]+=1;
+ryan[2]='Gotham';
+reuben.splice(2,1,'Chicago');
+jim.splice(2,1,'San Diego','Boston','Austin');
+jim.splice(2,1);
+console.log(shahzad);
+console.log(jim);
+console.log(reuben);
+console.log(daniel);
+console.log(ryan);
 
 
 
